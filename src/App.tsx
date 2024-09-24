0a9aaf31f478513d1ed712b040ec70e982102b66
seongahong <girl0930h@gmail.com>
2024-09-24 13:23:07 +0900

    [add] 폰트적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,21 @@
+import { useState } from "react";
+import { twMerge } from "tailwind-merge";
+
 export default function App() {
-  return <div className="text-3xl font-bold underline">App</div>;
+  const [active, setActive] = useState<boolean>(false);
+  return (
+    <>
+      <h1 className="sofadi-one-regular">Hello, world</h1>
+      <h1 className="eve-condom">Hello, world</h1>
+      <button
+        className={twMerge(
+          "p-2 border border-gray-500 rounded-md bg-black text-white",
+          active && "bg-blue-500 border-blue-500"
+        )}
+        onClick={() => setActive(!active)}
+      >
+        클릭
+      </button>
+    </>
+  );
 }
